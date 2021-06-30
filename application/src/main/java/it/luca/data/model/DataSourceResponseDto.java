a7307c1a114ed6dd86dee88c961616ee9c15317a
carloniluca91 <carloniluca91@gmail.com>
2021-06-30 14:52:36 +0200

    small changes on response printing

--- application/src/main/java/it/luca/data/model/DataSourceResponseDto.java
+++ application/src/main/java/it/luca/data/model/DataSourceResponseDto.java
@@ -13,4 +13,14 @@ public class DataSourceResponseDto {
     private String ingestionOperationMessage;
     private String httpStatus;
     private String httpStatusDescription;
+
+    @Override
+    public String toString() {
+
+        return String.format("  messageTs: %s\n", messageTs)
+                .concat(String.format("  dataSourceId: %s\n", dataSourceId))
+                .concat(String.format("  httpStatus: %s\n", httpStatus))
+                .concat(String.format("  ingestionOperationCd: %s\n", ingestionOperationCode))
+                .concat(String.format("  ingestionOperationMsg: %s\n", ingestionOperationMessage));
+    }
 }
